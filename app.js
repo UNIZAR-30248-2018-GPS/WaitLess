158cc4d68261a59ed3bc0363f9017959b051b75c
Ivan Escuin <escuin100@gmail.com>
2018-11-07 18:15:12 +0100

    API para obtener la carta. WIP diseño API confirmar pedido.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const expressVue = require('express-vue');
 const YAML = require('yamljs');
 const swaggerDoc = YAML.load('./WaitLess-swagger/api/swagger/swagger.yaml');
 const app = express();
+const port = 3030;
 
 const routerCarta = require('./server/routes/carta');
 app.use('/docs',swaggerUI.serve, swaggerUI.setup(swaggerDoc));
@@ -11,4 +12,6 @@ app.use('/swagger-raw',express.static('WaitLess-swagger/api/swagger/swagger.yaml
 app.use('/carta',routerCarta);
 
 //Lanzamos el servidor
-app.listen(3030);
+app.listen(port, function () {
+    console.log("Servidor escuchando en puerto "+port);
+});
--- server/database/querys.js
+++ server/database/querys.js
@@ -2,16 +2,19 @@
 const getAllCarta = function () {
     return [{
         "nombre":'Plato 1',
+        "id":'id1',
         "precio":0.1,
         "tipo":0
     },
     {
         "nombre":'Plato 2',
+        "id":'id2',
         "precio":2,
         "tipo":1
     },
     {
         "nombre":'Plato 3',
+        "id":'id3',
         "precio":0.1,
         "tipo":2
     }];
