f99e80d32b933eddadfc99c319f13531ec7dac80
crzyivo <ivan.nadie@gmail.com>
2018-11-17 17:50:01 +0100

    GET de carta parametrizado. Parametro 'tipo' en query de URL, mandando un string [ plato, bebida, menu1, menu2, postre ].

--- server/database/querys.js
+++ server/database/querys.js
@@ -1,23 +1,65 @@
 //TODO: Interaccion con la base de datos, por ahora las funciones sirven de mock
-const getAllCarta = function () {
-    return [{
-        "nombre":'Plato 1',
-        "id":'id1',
-        "precio":0.1,
-        "tipo":0
-    },
-    {
-        "nombre":'Plato 2',
-        "id":'id2',
-        "precio":2,
-        "tipo":1
-    },
-    {
-        "nombre":'Plato 3',
-        "id":'id3',
-        "precio":0.1,
-        "tipo":2
-    }];
+const getAllCarta = function (tipoItem) {
+    if(!tipoItem) {
+      return [{
+        "nombre": 'Plato 1',
+        "id": 'id1',
+        "precio": 0.1,
+        "tipo": 0
+      },
+        {
+          "nombre": 'Plato 2',
+          "id": 'id2',
+          "precio": 2,
+          "tipo": 1
+        },
+        {
+          "nombre": 'Plato 3',
+          "id": 'id3',
+          "precio": 0.1,
+          "tipo": 2
+        }];
+    }else if(tipoItem===1){
+        //Busqueda con parametros, por ahora devuelvo algo distinto
+      return [{
+        "nombre": 'Bravas',
+        "id": 'id1',
+        "precio": 0.1,
+        "tipo": 1
+        }];
+    }else if(tipoItem===2){
+      //Busqueda con parametros, por ahora devuelvo algo distinto
+      return [{
+        "nombre": 'Vino',
+        "id": 'id1',
+        "precio": 0.1,
+        "tipo": 2
+      }];
+    }else if(tipoItem===3){
+      //Busqueda con parametros, por ahora devuelvo algo distinto
+      return [{
+        "nombre": 'Ensalada',
+        "id": 'id1',
+        "precio": 0.1,
+        "tipo": 3
+      }];
+    }else if(tipoItem===4){
+      //Busqueda con parametros, por ahora devuelvo algo distinto
+      return [{
+        "nombre": 'Guisado de ternera',
+        "id": 'id1',
+        "precio": 0.1,
+        "tipo": 4
+      }];
+    }else if(tipoItem===5){
+      //Busqueda con parametros, por ahora devuelvo algo distinto
+      return [{
+        "nombre": 'Helado de fresa',
+        "id": 'id1',
+        "precio": 0.1,
+        "tipo": 5
+      }];
+    }
 };
 
 const addPedido = function(pedido){
--- server/modules/carta/carta.js
+++ server/modules/carta/carta.js
@@ -1,9 +1,11 @@
 const Enum = require('enum');
 const bd = require('../../database/querys');
-const tipo = new Enum({'plato':0,'bebida':1,'menu':2},{ignoreCase: true});
+const tipo = new Enum({'plato':0,'bebida':1,'menu1':2,'menu2':3,'postre':4},{ignoreCase: true});
 
 const carta_get = function (req,res) {
-    var todaLaCarta = bd.getAllCarta();
+    var tipoItem = req.query.tipo && tipo.isDefined(req.query.tipo) ? tipo.get(req.query.tipo).value : undefined;
+    console.log(tipoItem);
+    var todaLaCarta = bd.getAllCarta(tipoItem);
     todaLaCarta.forEach(function (item){
         if(tipo.isDefined(item.tipo)) {
             item.tipo = typeof item.tipo === 'string' ? item.tipo : tipo.get(item.tipo).key;
