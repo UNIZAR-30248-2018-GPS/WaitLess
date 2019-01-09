f93e1953bd1b43760c733be980e75acbc0eaa24c
David Mañas <davidmavi16@gmail.com>
2019-01-09 18:22:36 +0100

    Documentación carta.js

--- server/modules/carta/carta.js
+++ server/modules/carta/carta.js
@@ -2,6 +2,11 @@ const Enum = require('enum');
 const bd = require('../../database/querys');
 const tipos = new Enum({'plato':0,'bebida':1,'menu1':2,'menu2':3,'postre':4},{ignoreCase: true});
 
+/**
+* Función que obtiene toda la carta realizando una conexión a la base de datos.
+* @params req
+* @params res 
+*/
 const carta_get = function (req,res) {
     var tipoItem = req.query.tipo && tipos.isDefined(req.query.tipo) ? tipos.get(req.query.tipo).value : undefined;
     console.log('Saca carta de '+tipoItem);
