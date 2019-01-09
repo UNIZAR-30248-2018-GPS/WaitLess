45ee29ab485265b66527d42e148ebaee425af48b
David Mañas <davidmavi16@gmail.com>
2019-01-09 18:43:14 +0100

    Documentación avisos.js

--- server/modules/servicio/avisos.js
+++ server/modules/servicio/avisos.js
@@ -2,24 +2,43 @@ const bd = require('../../database/querys');
 const Enum = require('enum');
 const aviso = new Enum({'sinaviso':0, 'pendiente':1,'visto':2},{ignoreCase: true});
 
+
+/**
+* Función que llama a la base de datos y obtiene todos los avisos activos de
+* llamada al camarero de cada pedido.
+* @param req
+* @param res
+**/
 const get_all_avisos = function (req,res) {
     bd.get_avisos(res);
    // Deberia mandar solo status 201 si se ha completado con exito
 };
 
+/**
+* Función que crea la estructura necesaria para activar un aviso de camarero
+* llamando a la base de datos.
+* @param req
+* @param res
+**/
 const call_camarero_avisos = function (req,res) {
     let data = [
         aviso.get('pendiente').value,
         req.params.mesa
-    ] 
+    ]
     bd.call_camarero_avisos(data,res);
    // Deberia mandar solo status 201 si se ha completado con exito
 };
 
+/**
+* Función que crea la estructura necesaria para activar un aviso de solicitud
+* de cuenta llamando a la base de datos.
+* @param req
+* @param res
+**/
 const pedir_cuenta = function (req,res) {
     let data = [
         req.params.num_pedido
-    ] 
+    ]
     bd.pedir_cuenta(data,res);
    // Deberia mandar solo status 201 si se ha completado con exito
 };
