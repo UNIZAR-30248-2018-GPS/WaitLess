d9aacc864caf10142ede21f0cceb8826c770ac6e
Ivan Escuin <escuin100@gmail.com>
2019-01-10 17:54:50 +0100

    Errores de conexion de carta

--- server/modules/carta/carta.js
+++ server/modules/carta/carta.js
@@ -1,6 +1,6 @@
 const Enum = require('enum');
 const bd = require('../../database/querys');
-const tipos = new Enum({'plato':0,'bebida':1,'menu1':2,'menu2':3,'postre':4},{ignoreCase: true});
+const tipos = new Enum({'bebida':1,'menu1':2,'menu2':3,'postre':4},{ignoreCase: true});
 
 /**
 * Función que obtiene toda la carta realizando una conexión a la base de datos.
@@ -11,6 +11,9 @@ const carta_get = function (req,res) {
     var tipoItem = req.query.tipo && tipos.isDefined(req.query.tipo) ? tipos.get(req.query.tipo).value : undefined;
     console.log('Saca carta de '+tipoItem);
     bd.getAllCarta(tipoItem,function (err,todaLaCarta) {
+        if(err){
+            res.status(500).send();
+        }
         todaLaCarta.forEach(function (item){
             if(tipos.isDefined(item.tipo)) {
                 item.tipos = typeof item.tipo === 'string' ? item.tipo : tipos.get(item.tipo).key;
